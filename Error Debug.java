34c130a4fad1bd35514653c347956d04f93872ad
shruthi-sai <89636224+shruthi-sai@users.noreply.github.com>
2021-09-09 21:37:26 +0530

    Add files via upload

---
+++ Error Debug.java
@@ -0,0 +1,19 @@
+
+
+public class Kcg
+
+{
+
+public static void main(String a[])
+
+{
+
+char a=’s’;    // variable a is already defined in method parameter // wrong quotation is used
+
+
+int s=95;     //  conversion of int to char is incompatible
+
+a=s;          //  conversion of int to char is incompatible
+}
+
+}
