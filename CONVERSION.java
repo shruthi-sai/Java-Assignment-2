af5dcb2c7ae988e22ab2260da8f96d1c4c857225
shruthi-sai <89636224+shruthi-sai@users.noreply.github.com>
2021-09-09 21:36:10 +0530

    CONVERSION.java

---
+++ CONVERSION.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class CONVERSION {
+
+	public static void main(String[] args) {
+		int a;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number to be converted:");
+		a = sc.nextInt();
+		long b = a;
+		float c = b;
+		System.out.println("Int to Long: " + b);
+		System.out.print("Long to Float: " + "" + c);
+		
+  
+
+	}
+
+}
+ 
